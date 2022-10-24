1fcafe34860a791f420cdcdd655160eb5fc5db66
davilos <daviloscostagg@hotmail.com>
2022-10-24 13:22:39 -0300

    tentando correção

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import cadastro, index, contato, produto
+from .views import cadastro, index, contato, produto, logar
 
 urlpatterns = [
     path('', index, name='index'),
     path('contato/', contato, name='contato'),
     path('produto/', produto, name='produto'),
     path('cadastro/', cadastro, name='cadastro'),
+    path('logar/', logar, name='logar'),
 ]
