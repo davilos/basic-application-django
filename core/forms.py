4e745ee917ad716437ae7c577bed8a52ead93e30
davilos <daviloscostagg@hotmail.com>
2022-10-24 13:52:16 -0300

    model logar removido2

--- core/forms.py
+++ core/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.core.mail.message import EmailMessage
-from .models import Produto, Cadastro, Logar
+from .models import Produto, Cadastro
 
 
 class ContatoForm(forms.Form):
